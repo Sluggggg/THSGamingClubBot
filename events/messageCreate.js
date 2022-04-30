a2922a608dbb3669a2029b3e71a07a8c0199f19b
Sluggggg <97277864+Sluggggg@users.noreply.github.com>
2022-04-29 20:52:11 -0700

    added some illegal words lol

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,13 @@
-/*module.exports = {
+//import { ILLEGAL } from "./other/illegalWords.js";
+
+module.exports = {
     name: 'messageCreate',
     once: true,
     async execute (message) {
         if (message.author.bot) return;
 
-        if (message.toLowerCase().includes('fuck' || 'shit' || 'dick' || 'ass' || 'asshole' || 'bitch' || 'cunt' || 'fucker' || 'bullshit' || 'cock' || 'cocksucker' || 'slut' || 'whore' || 'twat' || 'pussy' || 'tit' || 'titty' || 'titties' || 'vagina' || 'penis')) {
+        if (message.content.toLowerCase().includes(ILLEGAL)) {
             message.delete();
-        };
+        }
     }
-}*/
+}
